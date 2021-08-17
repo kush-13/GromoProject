a1da024b96a3999a7c28b166bbcbf30902a864b7
kush-13 <kush.arma1399@gmail.com>
2021-08-17 20:53:52 +0530

    fixed connection missing bug

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+connection = db.getConnection();
 
 app.get("/createQuiz/:topic", (req, res)=>{
-    let result = db.createQuiz(req.params.topic);
+    let result = db.createQuiz(req.params.topic, connection);
     if (result.error){
         res.sendStatus(400).send(result);
     }else{
