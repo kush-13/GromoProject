96904f036411315dfe944853cfa2b3123db4e7d7
kush-13 <kush.arma1399@gmail.com>
2021-08-17 20:49:37 +0530

    added api ~  /createQuiz/:topic

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./db');
 const mysql = require('mysql');
 
 const app = express();
@@ -6,6 +7,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+
+app.get("/createQuiz/:topic", (req, res)=>{
+    let result = db.createQuiz(req.params.topic);
+    if (result.error){
+        res.sendStatus(400).send(result);
+    }else{
+        console.log();
+        res.sendStatus(200);
+    }
+
+})
 
 app.listen('3000', ()=>{
     console.log("Server Listening on Port 3000")
