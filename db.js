217b1f380825aef130afc689aa2477a3cdf51ece
kush-13 <kush.arma1399@gmail.com>
2021-08-18 19:46:22 +0530

    added invalid quiz_id edge case to api ~ /addQuestion

--- db.js
+++ db.js
@@ -32,6 +32,11 @@ module.exports.createQuiz = (topic, connection)=>{
 module.exports.addQuestion = (quiz_id, brief, op1, op2, op3, op4, ans, connection)=>{
 
     return new Promise((resolve, reject)=>{
+        connection.query(`select * from quiz where quiz_id = ${quiz_id}`, (error, results, fields)=>{
+            if (error||results.length == 0){
+                reject(error||{error:"invalid quiz_id"});
+                }
+            });
         connection.query(`insert into questions (quiz_id, brief, op1, op2, op3, op4, ans) value (${quiz_id}, "${brief}", "${op1}", "${op2}", "${op3}", "${op4}", "${ans}")`, (error, results, fields)=>{
             if (error){
                 reject(error);
